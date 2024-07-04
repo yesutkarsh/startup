435eb6b9e68123da4bedbb129b803571ad49e68a
yesutkarsh <contactutkarshverma@gmail.com>
2024-07-04 22:01:07 +0530

    bugs fixed

---
+++ app/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./home.module.css"
+export default function HomePage() {
+  return (
+
+    <div className={style.wrapper}>
+      <div className={style.hero}>
+        <h1> Upload & <br />  <span>Print</span> </h1>
+
+        <span>India First Online Print On Demand Service</span> <br />
+      </div>
+    </div>
+  )
+}
